e3724a83289a35a83ab4ae0c45cf5ca95441a68d
Maximus7474 <the.maximus.7474@gmail.com>
2025-10-06 19:14:56 +0200

    feat(web/components): navbar element

---
+++ web/src/components/navbar.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+import { X } from 'lucide-react';
+import { Button } from './ui/button';
+
+export interface NavLink {
+  path: string;
+  name: string;
+}
+
+const Navbar: React.FC<{links: NavLink[], close: () => void}> = ({ links, close }) => {
+  const location = useLocation();
+
+  return (
+    <nav className="bg-gray-800 p-4 shadow-lg sticky top-0 z-10 w-full rounded-t-lg">
+      <div className="flex gap-2 justify-between items-center">
+        <div className="flex gap-2 justify-between items-start">
+          <p className="text-white text-2xl font-bold">
+            Admin Menu
+          </p>
+
+          <div className="flex space-x-4">
+            {links.map((link) => (
+              <Link
+                key={link.path}
+                to={link.path}
+                className={`
+                  px-3 py-2 rounded-md text-sm font-medium transition duration-300
+                  ${
+                    location.pathname === link.path
+                      ? 'bg-indigo-600 text-white shadow-md'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white'
+                  }
+                `}
+              >
+                {link.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        <Button variant='ghost' onClick={close} className='text-white'>
+          <X strokeWidth={3} />
+        </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
