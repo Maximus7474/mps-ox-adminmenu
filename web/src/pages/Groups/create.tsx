3fc176949c6865556510e810fde7b2f6307b4bdb
Maximus7474 <the.maximus.7474@gmail.com>
2025-10-10 13:06:27 +0200

    feat(web/pages): create group page placeholder

--- web/src/pages/Groups/create.tsx
+++ web/src/pages/Groups/create.tsx
@@ -1,7 +1,7 @@
-const GroupCreate: React.FC = () => {
-  return <div className="p-4">
-    <h1>Create Group</h1>
-  </div>;
+import GroupForm from "./components/groupForm";
+
+const GroupCreate = () => {
+  return <GroupForm title="Create Group" submit={(group) => alert(`Submitted: \n ${JSON.stringify(group, null, 4)}`)} submitError="Demo Error message for the system"/>
 }
 
 export default GroupCreate;
