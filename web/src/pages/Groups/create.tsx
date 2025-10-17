849543911f02c861a1b412afa5d8a16cae10e32a
Maximus7474 <the.maximus.7474@gmail.com>
2025-10-17 16:20:16 +0200

    feat(web/Groups): added better wrapping for group create

--- web/src/pages/Groups/create.tsx
+++ web/src/pages/Groups/create.tsx
@@ -1,7 +1,34 @@
-import GroupForm from "./components/groupForm";
+import { useState } from "react";
+import { fetchNui } from "../../utils/fetchNui";
+import GroupForm, { GroupData } from "./components/groupForm";
+
+import { type NuiFetchResponse } from "@common/types/nuiInterface";
+
+import { useNavigate } from "react-router-dom";
 
 const GroupCreate = () => {
-  return <GroupForm title="Create Group" submit={(group) => alert(`Submitted: \n ${JSON.stringify(group, null, 4)}`)} submitError="Demo Error message for the system"/>
+  const navigate = useNavigate();
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSubmit = (group: GroupData) => {
+    fetchNui<NuiFetchResponse<null>>('group', { action: 'create', data: group })
+    .then(response => {
+      if (response.success) {
+        navigate(`/groups/view/${group.name}/`);
+      } else {
+        setError(response.error);
+      }
+    })
+    .catch((err) => {
+      setError((err as Error).message);
+    });
+  };
+
+  return <GroupForm
+    title="Create Group"
+    submit={handleSubmit}
+    submitError={error}
+  />
 }
 
 export default GroupCreate;
