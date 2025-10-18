41022037a72a9c8f2f5fb59a3b7dbcb42233ad19
Maximus7474 <the.maximus.7474@gmail.com>
2025-10-18 17:26:21 +0200

    feat(web/Groups): added GroupView to page list export

--- web/src/pages/Groups/index.tsx
+++ web/src/pages/Groups/index.tsx
@@ -1,8 +1,10 @@
 import GroupCreate from "./create";
 import GroupsList from "./list";
+import GroupView from "./view";
 
 // format key value pairs, key being inner path to element
 export default {
   'view': GroupsList,
   'create': GroupCreate,
+  'view/:groupName': GroupView,
 };
