a4491db278f122699468d7651e39590c50605541
Maximus7474 <the.maximus.7474@gmail.com>
2025-10-20 17:10:59 +0200

    feat(web/Home): initial layout of homepage

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,6 +1,81 @@
+import { useEffect, useState } from "react";
+import { Separator } from "../components/ui/separator";
+import { type PlayerShortInfo } from "@common/types/players";
+import { fetchNui } from "../utils/fetchNui";
+import { ButtonGroup } from "../components/ui/button-group";
+import { Button } from "../components/ui/button";
+import { Loader2 } from "lucide-react";
+
+const maxplayers = 48, availablestaff = 5;
+
+const debugPlayerList: PlayerShortInfo[] = [];
+const groups = ['Police', 'Ambulance', 'Gang', 'Staff', undefined];
+for (let i = 1; i <= 25; i++) {
+  debugPlayerList.push({
+    source: `${i}`,
+    name: `Player ${i}`,
+    character: {
+      firstname: 'Firstname',
+      lastname: 'Lastname',
+      id: `${i}`,
+      stateid: window.crypto.randomUUID().split('-', 1)[0],
+      activegroup: groups[ i % groups.length ],
+    },
+  })
+}
+
 const Home: React.FC = () => {
-  return <div>
-    Home Page
+  const [playerlist, setPlayerList] = useState<PlayerShortInfo[]>([]);
+
+  useEffect(() => {
+    fetchNui<PlayerShortInfo[]>('home', { action: 'getplayers' }, { data: debugPlayerList, delay: 10_000 })
+    .then(setPlayerList)
+  })
+
+  return <div className="h-full flex flex-col px-4 py-2">
+    <h1 className="text-xl">Homepage</h1>
+
+    <Separator className="my-2" />
+
+    <div className="grid grid-cols-2 gap-4 mb-2">
+      <div></div>
+      <div className="flex flex-col">
+        <h2>Player list</h2>
+      </div>
+    </div>
+
+    <div className="grid grid-cols-2 gap-4 flex-1 overflow-auto">
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-2">
+          <p className="font-bold">Player Count:</p>
+          <span>{playerlist.length}/{maxplayers}</span>
+        </div>
+        
+        <div className="flex gap-2">
+          <p className="font-bold">Online Staff:</p>
+          <span>{availablestaff}</span>
+        </div>
+      </div>
+      <div className="flex flex-col space-y-2 p-2">        
+        {playerlist.length > 0
+          ? playerlist.map((player) => (
+            <div key={player.source} className="p-2 border rounded">
+              <div className="flex flex-row justify-between items-center">
+                <p>({player.source}) {player.name}</p>
+                <ButtonGroup>
+                  <Button variant='destructive'>Kick</Button>
+                  <Button variant='outline'>View</Button>
+                </ButtonGroup>
+              </div>
+            </div>
+          ))
+          : <div className="w-full flex gap-2 justify-center mt-6">
+              <Loader2 className="animate-spin" />
+              <p>Loading...</p>
+            </div>
+        }
+      </div>
+    </div>
   </div>
 };
 
