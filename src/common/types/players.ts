2d3aac73e380301fb8a0f20ddf8ee13ee78375b6
Maximus7474 <the.maximus.7474@gmail.com>
2025-10-26 13:23:05 +0100

    feat(common/types): player info and character info

--- src/common/types/players.ts
+++ src/common/types/players.ts
@@ -13,3 +13,32 @@ export interface PlayerShortInfo {
 export interface OnlinePlayerShortInfo extends PlayerShortInfo {
   source: number;
 }
+
+export interface PlayerInfo {
+  userId: number;
+  username: string;
+  license2: string;
+  steam: string|null;
+  fivem: string|null;
+  discord: string|null;
+  banned?: {
+    reason: string;
+    banned_at: number;
+    unban_at: number | null;
+  }
+}
+
+export interface PlayerCharacterShort {
+  charId: number;
+  stateId: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  lastPlayed: number;
+  isDead: 0 | 1;
+  deleted: 0 | 1;
+}
+
+export interface PlayerRecap extends PlayerInfo {
+  characters: PlayerCharacterShort[];
+}
