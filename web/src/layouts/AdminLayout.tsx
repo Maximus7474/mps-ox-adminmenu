f50a2f1795080d43bfd1b256b326ac44996bae6b
Maximus7474 <the.maximus.7474@gmail.com>
2026-03-02 18:41:16 +0100

    tweak(web/layout): reduced x padding

--- web/src/layouts/AdminLayout.tsx
+++ web/src/layouts/AdminLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet, useLocation } from 'react-router';
 import { SidebarProvider, SidebarInset, SidebarTrigger } from '../components/ui/sidebar';
-import { Separator } from '../components/ui/separator';
 import { AppSidebar, menuItems } from '../components/Sidebar';
 
 // AdminLayout.tsx
@@ -14,13 +13,12 @@ export function AdminLayout() {
       <SidebarInset className='flex flex-col min-w-0 flex-1 bg-background'>
         <header className='flex h-16 shrink-0 items-center gap-2 px-4 border-b border-border/10'>
           <SidebarTrigger className='-ml-1' />
-          <Separator orientation='vertical' className='mr-2 h-4' />
 
           <h1 className='text-sm font-semibold tracking-tight'>{currentPage?.title || 'Overview'}</h1>
         </header>
 
         <main className='flex flex-1 flex-col gap-4 p-4 pt-0'>
-          <div className='min-h-[100vh] flex-1 rounded-xl p-2'>
+          <div className='min-h-[100vh] flex-1 rounded-xl py-2'>
             <Outlet />
           </div>
         </main>
